90b9c390ada1d380310fe61d7d7f8df66950c8f5
Oluwatise Faith Alatise <oluwatisefaithalatise@Oluwatises-MacBook-Pro.local>
2018-08-29 14:10:05 -0400

    Faith added her files

---
+++ myscripts.js
@@ -0,0 +1,33 @@
+/***********************************************
+ * Team Profile JS
+ */
+//This controls the image slide show
+function openImage(evt, imageName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(imageName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+/***********************************************
+ * Game LiveStream JS
+ */
+
+ //This changes the channel ID for the twitch player
+ function changeChannelID()
+ {
+     let newid = document.getElementById("modaltwitchform").elements[0].value;
+     document.getElementById("twitchplayer").src= "https://player.twitch.tv/?channel=" + newid;
+ }
+
+ 
