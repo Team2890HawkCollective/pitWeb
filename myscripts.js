791ac74598ea48db9c0cb820f5dffabf3e8a813b
Oluwatise Faith Alatise <oluwatisefaithalatise@Oluwatises-MacBook-Pro.local>
2018-08-29 15:53:09 -0400

    Fix merge from Faith

--- myscripts.js
+++ myscripts.js
@@ -4,14 +4,18 @@
 //This controls the image slide show
 function openImage(evt, imageName) {
     var i, tabcontent, tablinks;
+   
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+   
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
+    //This displays the block image
     document.getElementById(imageName).style.display = "block";
     evt.currentTarget.className += " active";
 }
