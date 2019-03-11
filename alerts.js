5fb1a62e4f2fdcd0f8c7cda599cddee1ac625b1d
Kiersten Mitzel <kdmitzel43@yahoo.com>
2019-03-10 21:33:46 -0400

    Realized pit key was same for event. Modified so that it would intake event key.

--- alerts.js
+++ alerts.js
@@ -5,18 +5,20 @@ function updatoAlerto () {
 	eventKey = doc.elements[1].value;
     slackAPI = doc.elements[2].value;
     slackChannel = doc.elements[3].value;
-    matchNumber = doc.elements[4].value;
-    pitMap = doc.elements[5].value;
-    willDisplayData = doc.elements[6].checked;
+	matchNumber = doc.elements[4].value;
+    willDisplayData = doc.elements[5].checked;
     
     slackReader();
     storeUsers();
     updateInspection();
 }  
 
+/**
+ * Replaces src or embed to update to proper pit page
+ */
 function updateInspection()
 {
-    var eventName = "http://www.gafirst.org/peachpits/pitmap?event=" + pitMap;
+    var eventName = "http://www.gafirst.org/peachpits/pitmap?event=" + eventKey;
     var embed = document.getElementById("inspectionStatus");
     var copy = embed.cloneNode(true);
     copy.setAttribute('src', eventName);
